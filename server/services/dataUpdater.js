83a65714b2ac34bf09b8135ad1b91d3280ad5787
Mykyta Shupyliuk <Mykyta.Shupyliuk@corero.com>
2017-11-13 18:55:50 +0200

    modified dataUpdater

--- server/services/dataUpdater.js
+++ server/services/dataUpdater.js
@@ -6,9 +6,24 @@ class DataUpdater {
         console.log('fuck yeah');
     }
     static async faculties () {
-       const fac = await fetch('http://cist.nure.ua/ias/app/tt/get_faculties');
+       const fac = await fetch('http://cist.nure.ua/ias/app/tt/P_API_FACULTIES_JSON');
        const facJS = await fac.json();
        console.log(facJS);
+    }
+    static async teachers () {
+        const fac = await fetch('http://cist.nure.ua/ias/app/tt/P_API_PODR_JSON');
+        const facJS = await fac.json();
+        console.log(facJS);
+    }
+    static async groups () {
+        const fac = await fetch('http://cist.nure.ua/ias/app/tt/P_API_GROUP_JSON');
+        const facJS = await fac.json();
+        console.log(facJS);
+    }
+    static async events (id) {
+        const fac = await fetch(`http://cist.nure.ua/ias/app/tt/P_API_EVENT_JSON?timetable_id=${id}`);
+        const facJS = await fac.json();
+        console.log(facJS);
     }
 }
 
