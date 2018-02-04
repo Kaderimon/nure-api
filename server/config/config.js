146442e0784100653ee815f7d559f3d5096ae462
Kaderimon <schweppes2006@online.ua>
2018-02-04 04:55:51 +0200

    port change

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  port: process.env.port || 3001,
+  port: process.env.PORT || 3001,
   dbAddress: process.env.MONGODB_URI || `mongodb://localhost:${27017}`,
   apiSource: 'http://cist.nure.ua/ias/app/tt'
 }
