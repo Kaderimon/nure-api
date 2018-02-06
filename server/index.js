ce7ae437631931b61e52a2ef59e39b9bb921851c
Kaderimon <schweppes2006@online.ua>
2018-02-07 00:29:54 +0200

    bootstrap grid css and static to end

--- server/index.js
+++ server/index.js
@@ -8,23 +8,24 @@ import config from './config/config';
 import {routes, allowedMethods} from './routes/index.js';
 import { db } from './services/dbConnect';
 import cors from '@koa/cors';
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+
 const app = new Koa();
 const router = new Router().get('*', async (ctx, next) => {
   const indexHTML = fs.readFileSync(path.join(__dirname + '/../build/index.html'), 'utf-8');
 
   ctx.body = indexHTML;
-})
+});
+
 app.use(cors());
 app.use(logger());
 app.use(err);
 
 app.use(serve(path.join(__dirname + '/../build')));
-app.use(router.routes());
 app.use(routes());
 app.use(allowedMethods());
-
+app.use(router.routes());
 
 app.listen(config.port, function () {
   console.log('%s listening at port %d', 'nureApi', config.port);
